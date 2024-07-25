3cf8b1b3462fd82356376ce116e42712db5ed6e7
root <root@ip-172-31-11-31.ap-south-1.compute.internal>
2024-07-25 12:23:10 +0000

    commit 02

--- geo-cal.py
+++ geo-cal.py
@@ -2,8 +2,8 @@ import math
 
 class GeometryCalculator:
 
-    def calculate_circle_area(self, radius):
-        return math.pi * radius ** 2
+
+
 
     def calculate_rectangle_area(self, length, width):
         return length * width
